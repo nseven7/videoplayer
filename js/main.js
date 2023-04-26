30aa3252cfe2865ddd67b5ef32289063a2613296
nseven <57550221+nseven7@users.noreply.github.com>
2023-04-26 20:27:55 +0800

    add save time

--- js/main.js
+++ js/main.js
@@ -2,3 +2,23 @@ const player = new Plyr('#player', {
   keyboard: { focused: true, global: true },
   seekTime: 5,
 });
+
+
+// Сохраняем последнюю позицию в localStorage
+function saveLastWatched() {
+  localStorage.setItem("lastWatched", player.currentTime);
+}
+
+// Слушаем событие остановки воспроизведения
+player.on("pause", saveLastWatched);
+
+// Слушаем событие закрытия страницы
+window.addEventListener("beforeunload", saveLastWatched);
+
+// Получаем последнюю сохраненную позицию из localStorage
+const lastWatched = localStorage.getItem("lastWatched");
+
+// Устанавливаем последнюю позицию с помощью API Plyr
+if (lastWatched !== null) {
+  player.currentTime = lastWatched;
+}
